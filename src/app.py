dfb8b567d257bb34fbbbf1e345457595abce487a
SamuelHollis <samuelhollis16@gmail.com>
2024-09-10 11:07:07 +0200

    aetrrh

--- src/app.py
+++ src/app.py
@@ -108,7 +108,7 @@ h1 {
     font-weight: 700;
     text-align: center;
     margin-bottom: 15px;
-    opacity: 1;
+    opacity: 1.2;
     background-color: rgba(150, 150, 150, 0.4);
     padding: 4px;
     border-radius: 10px;
